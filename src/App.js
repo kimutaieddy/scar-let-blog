fd82a32e7429538f528704e14cbd37a17ec3b619
Kimutaieddy <eddykimutai@outlook.com>
2024-12-09 15:10:09 +0300

    Rename App component to todo and update content with new title, image, and list items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Let's get started!</h1>
-      </header>
-    </div>
-  );
-  
+function todo() {
+
+return (
+    <>
+        <div>
+            <h1>Practice without AI</h1>
+        </div>
+        <img 
+            src="https://theconversation.com/explainer-what-are-memes-20789"
+            alt="AI"
+            className="AI"
+        />
+        <ul>
+            <li>Invent new traffic lights</li>
+            <li>Rehearse a movie scene</li>
+            <li>Improve the spectrum technology</li>
+        </ul>
+    </>
+);
 }
 
-export default App;
+
+export default todo;
