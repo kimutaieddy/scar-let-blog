00cdc2ff52ee4c0be94c3e3bcff32a99ddfbbc59
Kimutaieddy <eddykimutai@outlook.com>
2024-12-10 19:59:45 +0300

    Add Home component and update App component structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from './Home';
 
 function App() {
-  return (
+   return (
     <Router>
       <header>
         <nav style={{
