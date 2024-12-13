40c8b173bf24dd5a98c7417109de5a907f7950ee
Kimutaieddy <eddykimutai@outlook.com>
2024-12-13 21:22:02 +0300

    Add Resources component with styling and content for accessing publications

--- Resources.jsx
+++ Resources.jsx
@@ -1,2 +1,18 @@
 import react from "react";
-impo
+import './Resources.css';
+
+function Resources() {
+    return (
+        <div>
+            <section className="section">  
+                <div className="container">
+                    <div className="card">
+                        <h3>Access Resources</h3>
+                        <p>CIS Kenya allows you to access tones of publications and reports on matters of Credit Information Sharing.</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+export default Resources;   
